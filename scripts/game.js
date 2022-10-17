e2a4779af3c6b872135f37626601e228f3565777
Erik1007 <erik.hartman1007@gmail.com>
2022-10-17 14:55:02 +0000

    developing simon game through jest testing, adding fucnitonsand testing if they work in stages

---
+++ scripts/game.js
@@ -0,0 +1,26 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "buton4"],
+}
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function addTurn () {
+    game.playerMoves = [];
+    game.currentGame.push (game.choices[(Math.floor(Math.random() * 4))]);
+    //showTurns ();
+}
+
+module.exports = { game, newGame, showScore, addTurn };
